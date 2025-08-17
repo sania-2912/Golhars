d7c23cc9012da702588c31c0ea02bc2e19d29eb0
Sania <sania.golhar@gmail.com>
2025-08-17 09:53:25 +0530

    UI fixes

--- server/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createProduct, getAllProducts } = require('../controllers/productController');
-
-router.post('/', createProduct);
-router.get('/', getAllProducts);
-
-module.exports = router;
